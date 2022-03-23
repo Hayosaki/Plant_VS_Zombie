7f00c0e29b81fa7e9d20043af860062af4121f26
Hayosaki <2563451660@qq.com>
2022-03-23 18:30:02 +0800

    初始化项目

---
+++ Assets/Script/GameManagement.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagement : MonoBehaviour
+{
+    public Grid grid;
+    public SceneManagement sceneManagement;
+    public ZombieManagement zombieManagement;
+}
